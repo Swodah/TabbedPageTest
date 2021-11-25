8d2dc4435d11914d3796bf103c9421cb76fa6bfb
Kevin Petersen <kpe@inLogic.dk>
2021-11-25 12:09:26 +0100

    Add project files.

---
+++ TabbedPageTest/TabbedPageTest/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TabbedPageTest.Models;
+using TabbedPageTest.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedPageTest.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
